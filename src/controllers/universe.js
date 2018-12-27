9e153867da7ab8baf4071d7e9820a866e568506e
Avinash <avinashb97@gmail.com>
2018-12-28 01:10:26 +0530

    List families in a universe

--- src/controllers/universe.js
+++ src/controllers/universe.js
@@ -1,4 +1,5 @@
 const Universe = require('../models/universe');
+const Family = require('../models/family');
 
 const create = async (req, res) => {
   let universe;
@@ -18,6 +19,35 @@ const create = async (req, res) => {
   });
 };
 
+const getFamilies = async (req, res) => {
+
+  const { universe } = req.body;
+
+  let families;
+  try {
+    families = await Family.findAll({ where: { universe } });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Internal Server Error'
+    });
+  }
+
+  if (!families.length) {
+    res.status(404).json({
+      message: `No families in the Universe ${universe}`
+    });
+    return;
+  }
+
+  res.status(200).json({
+    message: `List of families in the Universe ${universe}`,
+    data: { families }
+  });
+
+};
+
 module.exports = {
-  create
+  create,
+  getFamilies
 };
--- src/routes/universe.js
+++ src/routes/universe.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const universeController = require('../controllers/universe');
 
 router.post('/', universeController.create);
+router.get('/families', universeController.getFamilies);
 
 module.exports = router;
